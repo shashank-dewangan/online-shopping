24a9373f39b365ad674a2066ce5eada0a37bd40b
shashank dewangan <shashank.at30@gmail.com>
2020-11-18 15:14:02 +0530

    register user

--- backend/controllers/user.controller.js
+++ backend/controllers/user.controller.js
@@ -39,3 +39,28 @@ export const getUserProfile = asyncHandler(async (req, res) => {
     throw new Error(`User Not Found`);
   }
 });
+
+export const registerUser = asyncHandler(async (req, res) => {
+  const { name, email, password } = req.body;
+
+  const existingUser = await User.findOne({ email });
+
+  if (existingUser) {
+    res.status(401);
+    throw new Error('User Already Exist');
+  }
+  const createdUser = await User.create({ name, email, password });
+
+  if (createdUser) {
+    res.status(201).json({
+      _id: createdUser._id,
+      name: createdUser.name,
+      email: createdUser.email,
+      isAdmin: createdUser.isAdmin,
+      token: generateToken(createdUser._id),
+    });
+  } else {
+    res.status(401);
+    throw new Error('Invalid data');
+  }
+});
