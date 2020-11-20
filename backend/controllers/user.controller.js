cbd90ad8e2efe9f7e9faac87215ddb55bee8f1b0
shashank dewangan <shashank.at30@gmail.com>
2020-11-20 18:46:19 +0530

    update user end point

--- backend/controllers/user.controller.js
+++ backend/controllers/user.controller.js
@@ -64,3 +64,29 @@ export const registerUser = asyncHandler(async (req, res) => {
     throw new Error('Invalid data');
   }
 });
+
+export const updateUserProfile = asyncHandler(async (req, res) => {
+  const user = await User.findById(req.user._id);
+  if (user) {
+    user.name = req.body.name || user.name;
+    user.email = req.body.email || user.email;
+    if (req.body.password) user.password = req.body.password || user.password;
+
+    const updatedUser = await user.save();
+    if (updatedUser) {
+      res.status(201).json({
+        _id: updatedUser._id,
+        name: updatedUser.name,
+        email: updatedUser.email,
+        isAdmin: updatedUser.isAdmin,
+        token: generateToken(updatedUser._id),
+      });
+    } else {
+      res.status(401);
+      throw new Error(`Invalid Data`);
+    }
+  } else {
+    res.status(404);
+    throw new Error('User Not Found');
+  }
+});
--- backend/routes/user.routes.js
+++ backend/routes/user.routes.js
@@ -3,12 +3,16 @@ import {
   authUser,
   getUserProfile,
   registerUser,
+  updateUserProfile,
 } from '../controllers/user.controller.js';
 import { protect } from '../middleware/auth.middleware.js';
 const router = express.Router();
 
 router.post('/', registerUser);
 router.post('/login', authUser);
-router.route('/profile').get(protect, getUserProfile);
+router
+  .route('/profile')
+  .get(protect, getUserProfile)
+  .put(protect, updateUserProfile);
 
 export default router;
